1db1d51cbab0b5303b092b3f61b1a3a61e270ba6
Tabisile <74783027+Tabisile@users.noreply.github.com>
2021-10-26 11:07:29 +0200

    Update task9.py

--- task9.py
+++ task9.py
@@ -1,5 +1,12 @@
-def vowels_in_a_string(name):
-    for vowel in name:
-        if vowel in "AEIOUaeiou":
-           print(vowel)
+def vowels_in_a_string(word):
+    final_name =word.lower()
+    list1 = list(word)
+    list2 = []
+    vowels = "aeiou"
+    for i in list1:
+        if i.lower() in vowels:
+            if i in vowels:
+                list2.append(i)
+    vowels_ouput = ', '.join(list2)
+    return vowels_ouput
 
